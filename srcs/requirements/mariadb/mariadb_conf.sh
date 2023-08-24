98d55c10525d53df021ad16397a98071a0881020
cjsauvain <cjsauvain@gmail.com>
2023-08-24 21:41:29 +0200

    inception

--- srcs/requirements/mariadb/mariadb_conf.sh
+++ srcs/requirements/mariadb/mariadb_conf.sh
@@ -8,6 +8,6 @@ Y
 Y
 EOF
 
-CREATE DATABASE testDatabase DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON wordpress.* TO 'champi'@'localhost' IDENTIFIED BY 'Bonjour456-!';
+CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON wordpress.* TO 'champi'@'localhost' IDENTIFIED BY 'Bonjour456-';
 FLUSH PRIVILEGES;
