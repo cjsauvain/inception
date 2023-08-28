c1e2cf34f7fc1e64129e38e783514039272c874b
cjsauvain <cjsauvain@gmail.com>
2023-08-28 22:51:23 +0200

    inception

--- srcs/requirements/nginx/conf/ssl_conf.sh
+++
@@ -1,7 +0,0 @@
-FR
-Angouleme
-Angouleme
-42 Angouleme
-42 Angouleme
-jsauvain
-jordansauvain42@gmail.com
--- srcs/requirements/wordpress/conf/config.sh
+++
@@ -1,3 +0,0 @@
-sed -i -r "s/.*DB_NAME.*/define( 'DB_NAME', 'wordpress' )/g" wp-config.php
-sed -i -r "s/.*DB_USER.*/define( 'DB_USER', 'champi' )/g" wp-config.php
-sed -i -r "s/.*DB_PASSWORD.*/define( 'DB_PASSWORD', 'Bonjour456-' )/g" wp-config.php
